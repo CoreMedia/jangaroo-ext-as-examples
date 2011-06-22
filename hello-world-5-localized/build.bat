13b4b81bf3ae9dafa13db655c19fc306a67db215
Frank Wienberg <frank.wienberg@coremedia.com>
2011-06-22 23:07:11 +0200

    Minor corrections on the first no-Maven build file, and another, full-featured build file for "hello-world-5-localized", this time even using the properties compiler propc.

--- hello-world-5-localized/build.bat
+++ hello-world-5-localized/build.bat
@@ -8,6 +8,7 @@ cd webapp
 cd ..
 type %JOO_HOME%\meta\joo\jangaroo-runtime.module.js %JOO_HOME%\meta\joo\jangaroo-browser.module.js %JOO_HOME%\meta\joo\ext-js.module.js %JOO_HOME%\meta\joo\ext-as.module.js > webapp\joo\jangaroo-application.js
 xcopy /s /y src\main\resources webapp\
-call %JOO_HOME%\bin\exmlc hello-world-3-exml acme tmp\hello-world-3-exml.xsd src\main\joo tmp\joo %JOO_HOME%\meta\ext3.xsd
+call %JOO_HOME%\bin\exmlc hello-world-5-localized acme tmp\hello-world-5-localized.xsd src\main\joo tmp\joo %JOO_HOME%\meta\ext3.xsd
+call %JOO_HOME%\bin\propc src\main\joo tmp\joo
 mkdir webapp\joo\classes
-call %JOO_HOME%\bin\jooc -v -g source -classpath %JOO_HOME%\libs\jangaroo-runtime.jar;%JOO_HOME%\libs\jangaroo-browser.jar;%JOO_HOME%\libs\ext-as.jar -d webapp\joo\classes -sourcepath tmp\joo tmp\joo\com\acme\extas\exml\HelloWorld.as
+call %JOO_HOME%\bin\jooc -v -g source -classpath %JOO_HOME%\libs\jangaroo-runtime.jar;%JOO_HOME%\libs\jangaroo-browser.jar;%JOO_HOME%\libs\ext-as.jar -d webapp\joo\classes -sourcepath src\main\joo;tmp\joo src\main\joo\com\acme\extas\localized\*.as tmp\joo\com\acme\extas\localized\*.as
